f3e9389764a30ff1c5ff6776dbc268305cf3c42d
Shin-Yan <sdfghlklk@gmail.com>
2022-06-04 17:33:54 +0800

    add disasm real functionality

--- disasm.cpp
+++ disasm.cpp
@@ -1,5 +1,34 @@
 #include "disasm.h"
 
+void disasm_all(unsigned long long address){
+    unsigned long long offset = address - loaded_program.asm_address;
+    unsigned char* pos = (unsigned char*)loaded_program.asm_code + offset;
+    csh h;
+    cs_insn* insn;
+    cs_open(CS_ARCH_X86,CS_MODE_64,&h);
+    if(cs_disasm(h,pos,100,address,0,&insn) >0){
+        for(int i = 0 ; i < DISASM_MAX_INSTRUCTIONS; i++){
+            if(!addr_in_text(insn[i].address)){
+                cout << "** the address is out of the range of the text segment" << endl;
+                cs_close(&h);
+                return;
+            }
+            printf("%12lx: ", insn[i].address);
+            for (size_t j = 0; j < 12; j++) {
+                if (j < insn[i].size) {
+                    printf("%02hhx ", insn[i].bytes[j]);
+                }
+                else{
+                    printf("   ");
+                }
+            }   
+            printf("\t%s\t%s\n", insn[i].mnemonic,insn[i].op_str);
+        }
+    }
+    cs_close(&h);
+    return;
+}
+
 int disasm(string address){
     if(address == ""){
         cout << "** no addr is given" <<endl;
@@ -14,10 +43,10 @@ int disasm(string address){
         cout << "** no addr is given" <<endl;
         return 0;
     } 
-    if((disasm_address < loaded_program.loaded_elf.entry_point) ||
-    (disasm_address >= loaded_program.loaded_elf.entry_point + loaded_program.loaded_elf.size)){
+    if(!addr_in_text(disasm_address)){
         cout << "** the address is out of the range of the text segment" << endl;
         return 0;
     }
+    disasm_all(disasm_address);
     return 0;
 }
--- disasm.h
+++ disasm.h
@@ -1,13 +1,17 @@
 #ifndef _DISASM_H
 #define _DISASM_H
 
+#define DISASM_MAX_INSTRUCTIONS 10
+
 #include "execmd.h"
 #include "break.h"
+#include "cont.h"
 
 #include<string>
 #include<iostream>
 using namespace std;
 
+void disasm_all(unsigned long long);
 int disasm(string);
 
 #include <capstone/capstone.h>
